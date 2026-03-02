376d781b34abab0eca9b6328640cec9eb198a8e9
josuecasadiegos <josuecasadiegos@gmail.com>
2026-03-02 01:24:04 -0500

    fixing bugs

--- app/verify/page.js
+++ app/verify/page.js
@@ -6,7 +6,6 @@ export default function VerifyPage() {
   return (
     <Suspense fallback={
       <div className="min-h-screen bg-gradient-to-br from-gray-950 via-gray-900 to-black flex items-center justify-center">
-        {/* Puedes usar un Loader2 aquí también, pero como es server, mejor texto simple o nada */}
         <p className="text-white text-2xl">Cargando...</p>
       </div>
     }>
