19712e9cc6a6981d72025b4e5215535c3302e2f4
josuecasadiegos <josuecasadiegos@gmail.com>
2026-03-20 16:35:13 -0500

    fixing bugs

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,11 @@
+'use client';
 import { Inter } from 'next/font/google';
 import { ThemeProvider } from 'next-themes';
 import './globals.css';
+
+
+import { Toaster } from 'react-hot-toast';
+import SocketListener from '@/components/SocketListener';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -13,6 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning>
       <body className={`${inter.className} bg-background text-foreground antialiased`}>
+        <Toaster position="top-right" />
+        
+        {/* 🔥 ESCUCHA GLOBAL DE SOCKET */}
+        <SocketListener />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
